f065bba84236db05b4c7948ae5736c1644a32ef4
jyothishnet <jyothishnet@gmail.com>
2014-10-12 23:20:23 +0530

    Adding modifications

--- plot1.R
+++ plot1.R
@@ -3,9 +3,16 @@ library(dplyr);
 ## Read the file and apply filter on Date
 
 hData <- read.table("G:\\Data Science\\Exdata\\household_power_consumption.txt",header=T,
-                    sep=";")
+                    sep=";",stringsAsFactors = FALSE,na.strings="?")
 hData <- mutate(hData,DateTime=paste(Date,Time));
 
-hData <- mutate(hData,Date=as.Date(Date,"%d/%m/%Y"))
+hData <- mutate(hData,Date=as.Date(Date,"%d/%m/%Y"));
+hData <- mutate(hData,DateTime=as.Date(Date,"%d/%m/%Y"));
 
-hDate <- filter(hData, Date =='2020-12-16')
+hDate <- filter(hData, DateTime >='2007-02-01', DateTime <= '2007-02-02');
+hDate <- hDate[complete.cases(hDate),]
+
+rm(hData);
+
+hist(as.numeric(hDate$Global_active_power),breaks=15,col="red",
+     xlab="Global Active Power (killowatts)",ylab="Frequency",main="Global Active Power")
---
+++ plot2.R
@@ -0,0 +1,19 @@
+
+library(dplyr);
+## Read the file and apply filter on Date
+
+hData <- read.table("G:\\Data Science\\Exdata\\household_power_consumption.txt",header=T,
+                    sep=";",stringsAsFactors = FALSE,na.strings="?")
+
+hData <- mutate(hData,Date=as.Date(Date,"%d/%m/%Y"));
+hData <- mutate(hData,DateTime=paste(Date,Time));
+hData <- mutate(hData,DateTime=strftime(hData$DateTime,format="%Y-%m-%d %H:%M:%S",usetz=FALSE));
+
+hDate <- filter(hData, as.Date(DateTime) >='2007-02-01', as.Date(DateTime) <= '2007-02-02');
+hDate <- hDate[complete.cases(hDate),]
+
+#rm(hData);
+
+plot(as.Date(hDate$DateTime),as.numeric(hDate$Global_active_power),type="b")
+
+title(xlab="",ylab="Global Active Power (killowatts)")
